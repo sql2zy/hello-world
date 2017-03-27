fad186f30649f7246543edb915b99ade53bf801f
sql_zy <qlshi@ibatterycloud.com>
2017-03-27 10:20:17 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.youmeek.demo;
+
+/**
+ * Created by SQL on 2017-03-27.
+ */
+public class HelloWorld {
+
+    public static  void  main(String[] args){
+        System.out.println("hello world");
+    }
+}
